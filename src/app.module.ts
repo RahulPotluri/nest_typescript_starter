169cd6dec5181a613e14852916c90b0fc615e953
Rahul Potluri <rahulpotluri@Rahuls-MBP.attlocal.net>
2022-01-30 16:58:30 -0600

    adding nest Js basics

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './components/users/users.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+
+@Module({
+  imports: [UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware);
+  }
+
+}

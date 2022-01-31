fc90aaf66121f332017cd235bc0a19cd31e8eebd
Rahul Potluri <rahulpotluri@Rahuls-MBP.attlocal.net>
2022-01-30 21:13:19 -0600

    adding nest ND MONGO framework

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,23 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './components/users/users.module';
+import { UsersModule } from './modules/users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { MongoService } from './database/mongo.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, 
+    MongooseModule.forRootAsync({
+      useClass: MongoService
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule implements NestModule {
+
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(LoggerMiddleware);
